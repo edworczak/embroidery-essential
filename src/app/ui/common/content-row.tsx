7aa90624bf80cd44e33106b445d61208839e9b01
Ewa Dworczak <edworczak@gmail.com>
2024-09-23 20:21:01 +0200

    added content row

---
+++ src/app/ui/common/content-row.tsx
@@ -0,0 +1,24 @@
+type ContentRowProps = {
+    children: React.ReactNode;
+    alignItems?: 'row-center' | 'row-between' | 'row-end';
+}
+
+export default function ContentRow({ children, alignItems }: ContentRowProps) {
+    const getStyles = () => {
+        switch (alignItems) {
+            case 'row-center':
+                return 'items-center justify-center gap-4';
+            case 'row-between':
+                return 'items-center justify-between';
+            case 'row-end':
+                return 'items-center justify-end gap-4'
+            default:
+                return 'flex-col gap-4';
+        }
+    }
+    return (
+        <div className={`w-full flex px-8 py-4 ${getStyles()}`}>
+            {children}
+        </div>
+    );
+}
