298f58e6a8f882a0ff93c4521b34845dc5fa3a65
Ewa Dworczak <edworczak@gmail.com>
2024-09-24 17:30:30 +0200

    updated some configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,30 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        ocean: {
-          DEFAULT: "#006989",
-          100: "#0C7489",
-          200: "#119DA4",
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                ocean: {
+                    DEFAULT: "#006989",
+                    100: "#0C7489",
+                    200: "#119DA4",
+                },
+                sea: "#007090",
+                cloudy: "#EAEBED",
+                mist: "#A3BAC3",
+            },
+            borderRadius: {
+                DEFAULT: "50vh",
+            },
         },
-        sea: "#007090",
-        cloudy: '#EAEBED',
-        mist: '#A3BAC3',
-      },
-      borderRadius: {
-        DEFAULT: '50vh',
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
