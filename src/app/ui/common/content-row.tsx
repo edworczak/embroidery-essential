6af196dabdbda5d055ee696746e8c74900c19e07
Ewa Dworczak <edworczak@gmail.com>
2024-09-24 17:31:17 +0200

    updated css and added threads json

--- src/app/ui/common/content-row.tsx
+++ src/app/ui/common/content-row.tsx
@@ -1,24 +1,22 @@
 type ContentRowProps = {
     children: React.ReactNode;
-    alignItems?: 'row-center' | 'row-between' | 'row-end';
-}
+    alignItems?: "row-center" | "row-between" | "row-end";
+};
 
 export default function ContentRow({ children, alignItems }: ContentRowProps) {
     const getStyles = () => {
         switch (alignItems) {
-            case 'row-center':
-                return 'items-center justify-center gap-4';
-            case 'row-between':
-                return 'items-center justify-between';
-            case 'row-end':
-                return 'items-center justify-end gap-4'
+            case "row-center":
+                return "items-center justify-center gap-4";
+            case "row-between":
+                return "items-center justify-between";
+            case "row-end":
+                return "items-center justify-end gap-4";
             default:
-                return 'flex-col gap-4';
+                return "flex-col gap-4";
         }
-    }
+    };
     return (
-        <div className={`w-full flex px-8 py-4 ${getStyles()}`}>
-            {children}
-        </div>
+        <div className={`w-full flex px-4 py-4 ${getStyles()}`}>{children}</div>
     );
 }
