7261fb2492e54642ee6c6baddbbe55d1eaa998d5
Ewa Dworczak <edworczak@gmail.com>
2024-10-08 20:52:57 +0200

    added Polish dictionary

---
+++ src/app/[lang]/dictionaries.ts
@@ -0,0 +1,7 @@
+import 'server-only'
+
+const dictionaries = {
+  pl: () => import('./dictionaries/pl.json').then((module) => module.default),
+}
+
+export const getDictionary = async (locale) => dictionaries[locale]()
